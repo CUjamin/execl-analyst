974c59f48718dba18965a358041abd9e23cc3466
cujamin <chbnjamin@gmail.com>
2020-03-23 23:14:55 +0800

    first commit 3

--- src/excel/ExcelObject.py
+++ src/excel/ExcelObject.py
@@ -6,7 +6,13 @@ class ExcelObject:
         self.excel = pd.ExcelFile(file)
         self.sheet_names = self.excel.sheet_names
         self.sheet_total = len(self.excel.sheet_names)
+        self.sheets = []
+        for sheet_name in self.sheet_names:
+            sheet = pd.read_excel(file, sheet_name)
+            self.sheets.append(sheet)
 
     def print(self):
         print('sheet_names:', self.sheet_names)
-        print('sheet_len:',self.sheet_total)
+        print('sheet_len:', self.sheet_total)
+        for sheet in self.sheets:
+            print(sheet)
